080925c6fee4959075063aaf2c25a366f1c22217
lutfulloxon <xodiyevlutfulloxonuz@gmail.com>
2022-01-21 13:46:36 +0500

    Initial commit

---
+++ data/config.py
@@ -0,0 +1,8 @@
+import os
+CHANNELS=["@jstutorial","@pythontutorialuz"]
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  
+ADMINS = list(os.environ.get("ADMINS"))  
+IP = str(os.environ.get("ip")) 
+PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
+
+
